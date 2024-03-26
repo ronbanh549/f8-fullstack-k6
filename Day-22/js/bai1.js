ef296458ade33f1920b55978b7e97b4678fbaa3c
MINHCHI <93319143+Ca-mega3-151@users.noreply.github.com>
2024-03-26 17:35:43 +0700

    bai 1

---
+++ Day-22/js/bai1.js
@@ -0,0 +1,22 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (var i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function isPalindrome(num) {
+  return num.toString() === num.toString().split("").reverse().join("");
+}
+
+function nextPrimePalindrome(n) {
+  while (true) {
+    if (isPrime(n) && isPalindrome(n)) {
+      return n;
+    }
+    n++;
+  }
+}
+
+console.log(nextPrimePalindrome(401));
